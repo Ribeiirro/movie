fa7b729e9e24733d01bf5566ffc313f2f7de810b
Jose Ribeiro <joseribeirocsr@gmail.com>
2024-05-14 07:40:25 -0300

    Desafio contrução de uma aplicação node de filmes

---
+++ src/controllers/NotesController.js
@@ -0,0 +1,42 @@
+const knex = require("../database/knex");
+
+class NotesController {
+  async index(request, response) {
+    response.send("Hello Word");
+  };
+
+  async create(request, response) {
+    const { title, description, rating } = request.body;
+    const { user_id } = request.params;
+
+    await knex("movie_notes").insert({
+      title,
+      description,
+      rating,
+      user_id,
+    });
+
+    response.json();
+  };
+
+  async show(request, response) {
+    const { id } = request.params;
+
+    const note = await knex("movie_notes").where({ id }).first();
+
+    return response.json(note);
+  };
+
+  async delete(request, response) {
+    const { id } = request.params;
+
+    await knex("notes").where({ id }).delete();
+
+    return response.json();
+  };
+
+
+
+};
+
+module.exports = NotesController;
